35a9f43feb8372755805a1bcf4970a9eb9e7a52b
nevest <guilhermejcarlucci@hotmail.com>
2023-01-25 00:08:21 -0300

    sticker callback att

--- bot.py
+++ bot.py
@@ -12,16 +12,12 @@ app = Client(
     bot_token=getenv('TELEGRAM_BOT_TOKEN')
 )
 
-@app.on_message(filters.command('help'))
-async def help(client, message):
-    print(message.chat.username, message.text)
-    await message.reply('Este é o menu de ajuda:')
-
+#função menu
 @app.on_message(filters.command('menu'))
 async def menu(client, message):
     menu = ReplyKeyboardMarkup(
         [
-            ['/help', '/command'],
+            ['/help', '/commands'],
             ['1', '2', '3']
         ],
         resize_keyboard=True
@@ -31,25 +27,33 @@ async def menu(client, message):
         reply_markup=menu
         )
 
+#comandos do bot
+@app.on_message(filters.command('help'))
+async def help(client, message):
+    print(f"{message.chat.username}: {message.text}")
+    await message.reply('Este é o menu de ajuda:')
+
+@app.on_message(filters.command('/pic'))
+
 @app.on_message(filters.photo)
 async def hand_photo(client, message):
-    print(message.chat.username, message.text)
+    print(f"{message.chat.username}: Enviou uma foto")
     await message.reply('Que bela foto!')
 
 @app.on_message(filters.voice | filters.audio)
 async def hand_audio(client, message):
-    print(message.chat.username, message.text)
+    print(f"{message.chat.username}: Enviou um audio")
     await message.reply('Que bela voz!')
 
 @app.on_message(filters.sticker)
 async def hand_sticker(client, message):
-    print(message.chat.username, message.text)
-    await message.reply('Parece até o Paullinho, só manda figurinhas.')
+    print(f'{message.chat.username}: Enviou um sticker')
+    await app.send_sticker(message.chat.id, message.sticker.file_id)
 
 # deixar abaixo de todos os comandos.
 @app.on_message()
 async def messages(client, message):
-    print(message.chat.username, message.text)
-    await message.reply(message.text + '????')
+    print(f"{message.chat.username}: {message.text}")
+    await message.reply(f'Você enviou: {message.text}')
 
 app.run()
